23ffe1a1806ab67c6632d023e2d470b0d271a168
Seth Smith <seth@lfg.ac>
2025-12-18 09:27:36 -0800

    update

--- apps/ui/src/app.py
+++ apps/ui/src/app.py
@@ -38,11 +38,11 @@ lightbar = front_pcm.init_channel(5, label="Lightbar")
 rock_lights_bumper = front_pcm.init_channel(6, label="Rock Lights Bumper")
 rock_lights_rear = rear_pcm.init_channel(8, label="Rock Lights Rear")
 
-front_flashers_low = front_pcm.init_channel(31, label="Front Flashers Low")
-rear_flashers_low  = rear_pcm.init_channel(33, label="Rear Flashers Low")
+front_flashers_low = front_pcm.init_channel(32, label="Front Flashers Low")
+rear_flashers_low  = rear_pcm.init_channel(34, label="Rear Flashers Low")
 
-front_flashers_high = front_pcm.init_channel(32, label="Front Flashers High")
-rear_flashers_high  = rear_pcm.init_channel(34, label="Rear Flashers High")
+front_flashers_high = front_pcm.init_channel(31, label="Front Flashers High")
+rear_flashers_high  = rear_pcm.init_channel(33, label="Rear Flashers High")
 
 fog_lights = front_pcm.init_channel(2, label="Fog Lights")
 
@@ -57,6 +57,7 @@ Front_lights = switches.add(
             [grill_lp4_low],
             [grill_lp6_low, grill_lp4_low],
             [grill_lp6_high, grill_lp4_high, lightbar],
+            []
         ],
     )
 )
